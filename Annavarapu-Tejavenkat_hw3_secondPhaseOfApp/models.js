8ec51c2ec955f945c2dcaed62f2a0d5680055688
TejavenkatA99 <144980358+TejavenkatA99@users.noreply.github.com>
2023-10-30 23:24:03 -0400

    Create models.js

---
+++ Annavarapu-Tejavenkat_hw3_secondPhaseOfApp/models.js
@@ -0,0 +1,15 @@
+// models/dataModel.js
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Data', dataSchema);
